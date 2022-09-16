6422679fbee9047822c61e9081188739dcce3a1a
Tiago Cinto <tiago.cinto@gmail.com>
2022-09-16 15:12:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ LOG.setLevel(logging.INFO)
 @app.route("/")
 def home():
     html = r"""<h3>Diabetes prediction API.</h3>
-    Input for the /predict route looks like: (testing deploy)<br>
+    Input for the /predict route looks like: (testing deploy - part 02)<br>
     {{<br>
     'preg':1,<br>
     'plas':93,<br>
